8f4ab61fa4060a5d8eade463032bbd0ec0fe9799
Martin Peeters <martin.peeters@affinitic.be>
2018-09-09 22:23:30 +0200

    Update `Bounds` widget for eea.facetednavigation >= 10

--- src/collective/geo/faceted/widget/bounds.py
+++ src/collective/geo/faceted/widget/bounds.py
@@ -1,14 +1,22 @@
 # -*- coding: utf-8 -*-
+
 from collective.geo.faceted import _
 from collective.geo.faceted.widget.interfaces import IBoundsWidget
+from eea.facetednavigation.widgets import ViewPageTemplateFile
+from eea.facetednavigation.widgets.interfaces import DefaultSchemata
+from eea.facetednavigation.widgets.interfaces import ISchema
+from eea.facetednavigation.widgets.interfaces import LayoutSchemata
 from eea.facetednavigation.widgets.widget import Widget as AbstractWidget
-from Products.Five.browser.pagetemplatefile import ZopeTwoPageTemplateFile
 from zope.interface import implementer
 
 import logging
 
 
 logger = logging.getLogger('collective.geo.faceted.')
+
+
+class IBoundsSchema(ISchema):
+    pass
 
 
 @implementer(IBoundsWidget)
@@ -18,14 +26,13 @@ class Widget(AbstractWidget):
     # Widget properties
     widget_type = 'bounds'
     widget_label = _('Bounds selection')
-    view_js = '++resource++collective.geo.faceted.widget.view.js'
-    edit_js = '++resource++collective.geo.faceted.widget.edit.js'
-    view_css = '++resource++collective.geo.faceted.widget.view.css'
-    # edit_css = '++resource++collective.geo.faceted.widget.edit.css'
 
-    index = ZopeTwoPageTemplateFile('bounds.pt', globals())
-    edit_schema = AbstractWidget.edit_schema.copy()
-    edit_schema['title'].default = _('Boundary Bounds')
+    groups = (
+        DefaultSchemata,
+        LayoutSchemata,
+    )
+
+    index = ViewPageTemplateFile('bounds.pt')
 
     def __init__(self, context, request, data=None):
         super(Widget, self).__init__(context, request, data)
